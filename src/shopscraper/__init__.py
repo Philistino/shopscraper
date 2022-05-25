a35f640e5d2a153a1250c32f18f9a9d774d7d2f7
Philistino <Philistino@protonmail.com>
2022-05-25 18:24:03 +0100

    Edit in build settings

--- src/shopscraper/__init__.py
+++ src/shopscraper/__init__.py
@@ -4,25 +4,8 @@ from logging import NullHandler
 from shopscraper.api import read_json, scrape, scrape_to_json
 from shopscraper.prod_objs import Image, Product, Variant
 
+# Set default logging handler to avoid "No handler found" warnings.
 logging.getLogger(__name__).addHandler(NullHandler())
-
-
-# def add_stderr_logger(level: int = logging.DEBUG) -> logging.StreamHandler:
-#     """
-#     Helper for quickly adding a StreamHandler to the logger. Useful for
-#     debugging.
-#     Returns the handler after adding it.
-#     """
-#     # This method needs to be in this __init__.py to get the __name__ correct
-#     # even if urllib3 is vendored within another package.
-#     logger = logging.getLogger(__name__)
-#     handler = logging.StreamHandler()
-#     handler.setFormatter(logging.Formatter("%(asctime)s %(levelname)s %(message)s"))
-#     logger.addHandler(handler)
-#     logger.setLevel(level)
-#     logger.debug("Added a stderr logging handler to logger: %s", __name__)
-#     return handler
-
 
 # ... Clean up.
 del NullHandler
